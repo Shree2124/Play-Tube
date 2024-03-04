0cf0d8f56d8c7e2528e8a046ac6c737724ff9821
Shree2124 <shreealasande@gmail.com>
2024-03-04 23:08:32 +0530

    commit 14

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,39 +1,27 @@
-import express, { json } from "express";
-// import mongoose from "mongoose";
-// import { PORT, PROJECT_ID } from "./config.js";
-// import { Book } from "./models/books.models.js";
-// import booksRoutes from './routes/books.routes.js'
 import connectDB from "./db/index.js"
-import router from "./routes/user.routes.js"
-import cors from "cors"
+import express, { json } from "express"
+import dotenv from "dotenv"
+
+const app = express()
 
 
-const app = express();
-// const Port = PORT || 3000;
-
-app.use(express.json());
-// app.use(cors({
-//   origin: "http://localhost:5173",
-//   methods: ['GET', 'POST', 'PUT', 'DELETE'],
-//   allowedHeaders: ['Contenet-Type'],
-// }));
-
-// app.use(cors());
-
-app.use(cors({
-  origin: ['https://book-store-frontend-rho-two.vercel.app','http://localhost:5173',"*"],
-  methods: ["POST", "GET", "PUT", "DELETE"],
-  credentials: true
-}))
-// app.use('/users',router);
-
-app.get("/", (req, res) => {
-  res.send("hello");
-});
-
-
-app.listen(process.env.PORT, () => {
-  console.log(`serve at http://localhost:${process.env.PORT}`);
-});
+dotenv.config({
+    path: './.env'
+})
 
 connectDB()
+.then(()=>{
+    console.log("Successfully db connected");
+})
+.catch((error)=>console.log("DB connection error:- ",error));
+
+app.use(json())
+
+
+app.get("/",(req, res)=>{
+    res.send("hello")
+})
+
+app.listen(process.env.PORT,()=>{
+    console.log("Server is running on http://localhost:8000");
+})
