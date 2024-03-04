b0a0e04e7eeee3da3c479118df7b3eb580878bbb
Shree2124 <shreealasande@gmail.com>
2024-03-04 20:48:11 +0530

    commit 06

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,30 +1,34 @@
 // import mongoose from "mongoose";
 // import {DB_NAME} from './constants'
 // require('dotenv').config({path: './env'})
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 import connectDB from "./db/index.js";
-import { app } from './app.js';
+import { app } from "./app.js";
 
 dotenv.config({
-    path: './.env'
-})
-connectDB()
-.then(()=>{
-    app.listen(process.env.PORT || 8000, ()=>console.log(`Server is running at port : ${process.env.PORT}`))
-})
-.catch((error)=>console.log("MONGODB CONNECTION ERROR in index.js: ",error))
+  path: "./.env",
+});
 
-app.use(
-    cors({
-      origin: process.env.CORS_ORIGIN,
-      credentials: true,
-    })
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () =>
+      console.log(`Server is running at port : ${process.env.PORT}`)
+    );
+  })
+  .catch((error) =>
+    console.log("MONGODB CONNECTION ERROR in index.js: ", error)
   );
 
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
 
-
-
-
+app.get("/", (req, res)=>{
+    res.send("Hello")
+})
 
 /*
 // TODO: ⁡⁢⁢⁢first approch to connect db
