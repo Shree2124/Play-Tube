34920fe6063eb59813aa9e13f297be0b10cdbb21
Shree2124 <shreealasande@gmail.com>
2024-03-05 21:07:38 +0530

    commit 31 cors-4

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/api":"https://play-tube-api.vercel.app"
+      "/":"https://play-tube-api.vercel.app"
     }
   }
 })
