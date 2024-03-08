247d495331480460314e53088d9ff9e1f9db1d94
Shree2124 <shreealasande@gmail.com>
2024-03-08 16:08:01 +0530

    checking register api

--- Backend/routes/user.routes.js
+++ Backend/routes/user.routes.js
@@ -6,27 +6,29 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 
 const router = Router();
 router.post(
-    "/register",
-    asyncHandler(async (req, res)=>{
-        res.send("hello")
+  "/register",
+  asyncHandler(async (req, res) => {
+    // res.send("hello");
     const { fullName, email, username, password } = req.body;
-  //console.log("email: ", email);
+    //console.log("email: ", email);
 
-  if (
-    [fullName, email, username, password].some((field) => field?.trim() === "")
-  ) {
-    throw new apiError(400, "All fields are required");
-  }
+    if (
+      [fullName, email, username, password].some(
+        (field) => field?.trim() === ""
+      )
+    ) {
+      throw new apiError(400, "All fields are required");
+    }
 
-  const existedUser = await User.findOne({
-    $or: [{ username }, { email }],
-  });
+    const existedUser = await User.findOne({
+      $or: [{ username }, { email }],
+    });
 
-  if (existedUser) {
-    throw new apiError(409, "User with email or username already exists");
-  }
-  
-  /* const avatarLocalPath = req.files?.avatar[0]?.path;
+    if (existedUser) {
+      throw new apiError(409, "User with email or username already exists");
+    }
+
+    /* const avatarLocalPath = req.files?.avatar[0]?.path;
   console.log(req.files);
   //const coverImageLocalPath = req.files?.coverImage[0]?.path;
   // let avatarLocalPath;
@@ -59,25 +61,28 @@ router.post(
     throw new apiError(400, "Avatar file is required");
   } */
 
-  const user = await User.create({
-    fullName,
-    email,
-    password,
-    username,
-  });
+    const user = await User.create({
+      fullName,
+      email,
+      password,
+      username,
+    });
 
-  const createdUser = await User.findById(user._id).select(
-    "-password -refreshToken"
-  );
+    const createdUser = await User.findById(user._id).select(
+      "-password -refreshToken"
+    );
 
-  if (!createdUser) {
-    throw new apiError(500, "Something went wrong while registering the user");
-  }
+    if (!createdUser) {
+      throw new apiError(
+        500,
+        "Something went wrong while registering the user"
+      );
+    }
 
-  return res
-    .status(201)
-    .json(new apiResponse(200, createdUser, "User registered Successfully"));
-
-}));
+    return res
+      .status(201)
+      .json(new apiResponse(200, createdUser, "User registered Successfully"));
+  })
+);
 
 export default router;
